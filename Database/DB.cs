b51859a12819b2787388ee431b4f570c8c3ee5a8
billchetch <bill@chetch.net>
2019-12-23 18:57:25 +0800

    Namespace stuff

--- Database/DB.cs
+++ Database/DB.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
-namespace Database
+namespace Chetch.Database
 {
     //main database connection class.
     public class DB : IDisposable
